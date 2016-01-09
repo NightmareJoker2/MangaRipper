98d7e21e3327677857aecbffa446625dc6255389
NightmareJoker2 <NightmareJoker2@users.noreply.github.com>
2016-01-09 13:36:13 +0100

    getting rid of the remaining RegEx crap in the MangaHere chapter parser

--- MangaRipper.Core/Site/MangaHere/ChapterMangaHere.cs
+++ MangaRipper.Core/Site/MangaHere/ChapterMangaHere.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
+using HtmlAgilityPack;
 
 namespace MangaRipper.Core
 {
@@ -15,15 +16,36 @@ namespace MangaRipper.Core
         {
             var list = new List<Uri>();
             list.Add(Address);
-            Regex reg = new Regex(@"<option value=""(?<Value>http://www.mangahere.co/manga/[^""]+)"" (|selected=""selected"")>\d+</option>",
-                RegexOptions.IgnoreCase);
-            MatchCollection matches = reg.Matches(html);
+            HtmlDocument htmlDocument = new HtmlDocument() { };
+            htmlDocument.LoadHtml(html);
+            HtmlNodeCollection nodes = htmlDocument.DocumentNode.SelectNodes("//section[contains(@class, 'readpage_top')]/div[contains(@class, 'go_page')]/span[contains(@class, 'right')]/select/option");
 
-            foreach (Match match in matches)
+            //Regex reg = new Regex(@"<option value=""(?<Value>http://www.mangahere.co/manga/[^""]+)"" (|selected=""selected"")>\d+</option>",
+            //    RegexOptions.IgnoreCase);
+            //MatchCollection matches = reg.Matches(html);
+
+            foreach (HtmlNode node in nodes)
             {
-                var value = new Uri(Address, match.Groups["Value"].Value);
-                list.Add(value);
+                try
+                {
+                    string value = node.Attributes.Where(x => x.Name == "value").First().Value;
+                    if (value.StartsWith("//") || !value.Contains("://") || value.StartsWith("http://www.mangahere.co/manga/") || value.StartsWith("https://www.mangahere.co/manga/"))
+                    {
+                        Uri uri = new Uri(Address, value);
+                        list.Add(uri);
+                    }
+                }
+                catch
+                {
+
+                }
             }
+
+            //foreach (Match match in matches)
+            //{
+            //    var value = new Uri(Address, match.Groups["Value"].Value);
+            //    list.Add(value);
+            //}
 
             return list.Distinct().ToList();
         }
@@ -31,15 +53,32 @@ namespace MangaRipper.Core
         protected override List<Uri> ParseImageAddresses(string html)
         {
             var list = new List<Uri>();
-            Regex reg = new Regex("<img src=\"(?<Value>[^\"]+)\" onerror",
-                RegexOptions.IgnoreCase);
-            MatchCollection matches = reg.Matches(html);
+            HtmlDocument htmlDocument = new HtmlDocument() { };
+            htmlDocument.LoadHtml(html);
+            HtmlNodeCollection nodes = htmlDocument.DocumentNode.SelectNodes("//img[@id='image']");
 
-            foreach (Match match in matches)
+            //Regex reg = new Regex("<img src=\"(?<Value>[^\"]+)\" onerror",
+            //    RegexOptions.IgnoreCase);
+            //MatchCollection matches = reg.Matches(html);
+
+            foreach (HtmlNode node in nodes)
             {
-                var value = new Uri(Address, match.Groups["Value"].Value);
-                list.Add(value);
+                try
+                {
+                    Uri value = new Uri(Address, node.Attributes.Where(x => x.Name == "src").First().Value);
+                    list.Add(value);
+                }
+                catch
+                {
+
+                }
             }
+
+            //foreach (Match match in matches)
+            //{
+            //    var value = new Uri(Address, match.Groups["Value"].Value);
+            //    list.Add(value);
+            //}
 
             return list;
         }
